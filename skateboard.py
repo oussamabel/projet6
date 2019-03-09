dec60fd5bcce9cdb47f31ae84f3cb81ad57f8d8d
VerhoyeVictor <victor.verhoye@student.umons.ac.be>
2019-03-09 12:19:46 +0100

    petite modif f en gamma

--- skateboard.py
+++ skateboard.py
@@ -1,30 +1,30 @@
 from scipy.integrate import odeint as edo
 import numpy as np
 
-def solution(df, ddf, mu, m, t1, n, y0):
+def solution(dgamma, ddgamma, mu, m, t1, n, y0):
     """
     Methode afin de calculer la solution d'une EDO en un point.
     Cette methode retourne une liste de n couples (S(t), dS(t)) avec t variant
-    de 0 a t1. Elle depend des fonctions dxi (derivee de xi) et ddxi (derivee
-    seconde de xi) ainsi que d'un coefficient de frottements mu, une masse m
+    de 0 a t1. Elle depend des fonctions dgamma (derivee de gamma) et ddgamma (derivee
+    seconde de gamma) ainsi que d'un coefficient de frottements mu, une masse m
     et d'un point de depart y0.
     """
     t = np.linspace(0, float(t1),float(n))
-    return edo(fct, y0, t, args=(m, mu, df, ddf), mxstep=10000)
+    return edo(f, y0, t, args=(m, mu, dgamma, ddgamma), mxstep=10000)
 
-def fct(y, t, m, mu, df, ddf):
+def f(y, t, m, mu, dgamma, ddgamma):
     """
     Methode fct(t) afin de resoudre l'EDO depandant des parametres y0 (point de
     depart), m (la masse), mu (le coefficient de frottement), df (derivée de
     f) et ddf (derivee seconde de f).
     """
     s, ds = y
-    df_s = df(s)
-    ddf_s = ddf(s)
-    a = prod(df_s, ddf_s) * ds**2
-    b = 9.81 * df_s[1]
+    dgamma_s = dgamma(s)
+    ddgamma_s = ddgamma(s)
+    a = prod(dgamma_s, ddgamma_s) * ds**2
+    b = 9.81 * dgamma_s[1]
     c = (mu / m) * ds
-    d = prod(df_s, df_s)
+    d = prod(dgamma_s, dgamma_s)
     res = ((-a - b)/d) - c
     dydt = [ds, res]
     return dydt
