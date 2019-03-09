c6489b64138142b1b4ff56ef7477381fb16cbae6
Oussama Belhouari <161507@umons.ac.be>
2019-03-09 16:11:09 +0100

    Corrigé la doc et simplifié f

--- skateboard.py
+++ skateboard.py
@@ -1,30 +1,28 @@
+#-*- coding: utf-8 -*-
 from scipy.integrate import odeint as edo
 import numpy as np
 
 def solution(dgamma, ddgamma, mu, m, t1, n, y0):
     """
-    Methode afin de calculer la solution d'une EDO en un point.
-    Cette methode retourne une liste de n couples (S(t), dS(t)) avec t variant
-    de 0 a t1. Elle depend des fonctions dgamma (derivee de gamma) et ddgamma (derivee
-    seconde de gamma) ainsi que d'un coefficient de frottements mu, une masse m
-    et d'un point de depart y0.
+    Méthode afin de calculer la solution approchée d'une EDO.
+    Cette méthode retourne une liste de n couples (S(t), dS(t)) avec t variant
+    de 0 à t1. Elle dépend des fonctions dgamma (dérivée de gamma) et ddgamma
+    (derivée seconde de gamma) ainsi que d'un coefficient de frottement mu,
+    d'une masse m et d'un point de départ y0.
     """
-    t = np.linspace(0, float(t1),float(n))
+    t = np.linspace(0, t1, n)
     return edo(f, y0, t, args=(m, mu, dgamma, ddgamma), mxstep=10000)
 
-def f(y, t, m, mu, dgamma, ddgamma):
+def f(y0, t, m, mu, dgamma, ddgamma):
     """
-    Methode fct(t) afin de resoudre l'EDO depandant des parametres y0 (point de
-    depart), m (la masse), mu (le coefficient de frottement), df (derivée de
-    f) et ddf (derivee seconde de f).
+    Résout l'EDO selon les parametres y0 (point de départ), m (la masse), mu
+    (le coefficient de frottement), dgamma et ddgamma (voir plus haut).
     """
-    s, ds = y
-    dgamma_s = dgamma(s)
-    ddgamma_s = ddgamma(s)
-    a = np.dot(dgamma_s, ddgamma_s) * ds**2
-    b = 9.81 * dgamma_s[1]
-    c = (mu / m) * ds
-    d = np.dot(dgamma_s, dgamma_s)
-    res = ((-a - b)/d) - c
-    dydt = [ds, res]
+    S,dS=y0
+    c=np.dot(dgamma(S),ddgamma(S))
+    normcarre=np.dot(dgamma(S),dgamma(S))
+    k=c/normcarre
+    l=(9.81*dgamma(S)[1])/normcarre
+    z=(mu/m)
+    dydt=[dS,-(k*dS*dS)-l-(z*dS)]
     return dydt
